48f35b1be2598d6590c6ef8ee98c740492065bf3
Eshane Joseph <eshane.j@gmail.com>
2018-10-18 12:18:53 -0400

    storetypo

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-
+import configurestore from './store/configurestore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
