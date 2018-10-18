2d0c56d5e372bd2366c8289be489b914e2b4fbba
Eshane Joseph <eshane.j@gmail.com>
2018-10-17 23:23:25 -0400

    stable test 4

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
-import ConfigureStore from './store/ConfigureStore';
+import configureStore from './store/configureStore';
 
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
@@ -11,7 +11,7 @@ import getVisibleExpenses from './selectors/expenses';
 import { Provider } from 'react-redux';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store = ConfigureStore();
+const store = configureStore();
 
 
 
--- src/store/configurestore.js
+++ src/store/configurestore.js
@@ -1,16 +1,15 @@
 import { createStore, combineReducers } from 'redux';
 import expensesReducer from '../reducers/expenses';
-import filtersReducers from '../reducers/filters';
+import filtersReducer from '../reducers/filters';
 
 export default () => {
-
     const store = createStore(
         combineReducers({
             expenses: expensesReducer,
-            filters: filtersReducers
+            filters: filtersReducer
         }),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
-return store
+    return store;
 };
