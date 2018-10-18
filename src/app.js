682cdf014ad9414cb736310517a8580d29a6b39f
Eshane Joseph <eshane.j@gmail.com>
2018-10-18 12:32:02 -0400

    S

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import getVisibleExpenses from './selectors/expenses';
 import { Provider } from 'react-redux';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store = configureStore();
+const store = configurestore();
 
 
 
