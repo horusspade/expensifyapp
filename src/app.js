082ec0d57f7d67f6ab1640d53ab0483dacc12933
Eshane Joseph <eshane.j@gmail.com>
2018-10-17 20:45:20 -0400

    Con

--- src/app.js
+++ src/app.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
+import ConfigureStore from './store/ConfigureStore';
+
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import { Provider } from 'react-redux';
 import 'react-dates/lib/css/_datepicker.css';
 
-const store = configureStore();
-console.log('test');
+const store = ConfigureStore();
+
+
 
 const jsx = (
     <Provider store={store}>
