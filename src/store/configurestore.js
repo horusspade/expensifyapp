8a365c44f28bfa9ac9cfc197cdc50130a4cd5c67
Eshane Joseph <eshane.j@gmail.com>
2018-10-21 00:02:31 -0400

    so sleepyyy

--- src/store/configurestore.js
+++ src/store/configurestore.js
@@ -12,7 +12,7 @@ export default () => {
         }),
         composeEnhancers(applyMiddleware(thunk))
         //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
+    );//hi
 
     return store;
 };
